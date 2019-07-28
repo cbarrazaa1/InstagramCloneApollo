53561978343098812895c821684a1b2a3920ebfe
Cesar Barraza <cbarrazaa1@gmail.com>
2019-07-28 00:55:30 -0700

    added resolver for new name EntUser field

--- Server/src/resolvers/Types.js
+++ Server/src/resolvers/Types.js
@@ -5,6 +5,7 @@ export default {
   User: {
     id: (parent: EntUser): string => parent.getID(),
     username: (parent: EntUser): string => parent.getUsername(),
+    name: (parent: EntUser): string => parent.getName(),
     authToken: (parent: EntUser): string => parent.getAuthToken(),
   },
 };
